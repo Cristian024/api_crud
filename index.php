09eaaf37aedc9e37b7a13031bec35f3a2fe3784a
CristianRobledo0705 <cristiandavidrobledoleo@gmail.com>
2024-03-30 00:13:51 -0500

    Headers CORS

--- index.php
+++ index.php
@@ -11,7 +11,10 @@ try {
     die();
 }
 
-header("Content-type: application/json");
+header("Content-type: application/json; charset=utf-8");
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
 
 parse_str($_SERVER['QUERY_STRING'], $params);
 
